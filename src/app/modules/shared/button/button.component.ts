616ee44746712754930eed9be3572aab5c1dc8fe
Max Niu <maxniu444@gmail.com>
2026-09-06 17:45:23 -0400

    scaffold shared button and module

---
+++ src/app/modules/shared/button/button.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: false,
+  styleUrl: './button.component.css',
+  templateUrl: './button.component.html',
+})
+export class ButtonComponent {}
