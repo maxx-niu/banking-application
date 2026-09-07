86e8bad9b2b1f314ff9a07b1898236885517dba5
Max Niu <maxniu444@gmail.com>
2026-09-07 04:47:24 -0400

    add date and amount range filters for the transaction history view

--- src/app/modules/features/transactions/transaction-history/transaction-history.component.ts
+++ src/app/modules/features/transactions/transaction-history/transaction-history.component.ts
@@ -1,8 +1,27 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, inject, input, signal } from '@angular/core';
+import {
+  NonNullableFormBuilder,
+  type AbstractControl,
+  type ValidationErrors,
+} from '@angular/forms';
 
 import { AccountService } from '@app/services/account.service';
 
 import type { ITransaction } from '@app/types';
+
+interface ITransactionFilters {
+  minAmount: string;
+  maxAmount: string;
+  fromDate: string;
+  toDate: string;
+}
+
+const NO_FILTERS: ITransactionFilters = {
+  minAmount: '',
+  maxAmount: '',
+  fromDate: '',
+  toDate: '',
+};
 
 @Component({
   selector: 'app-transaction-history',
@@ -11,11 +30,57 @@ import type { ITransaction } from '@app/types';
 })
 export class TransactionHistoryComponent {
   private accountService = inject(AccountService);
+  private fb = inject(NonNullableFormBuilder);
+
+  private readonly appliedFilters = signal<ITransactionFilters>(NO_FILTERS);
 
   readonly id = input.required<string>();
 
+  // Filter validator
+  private rangeValidator = (group: AbstractControl): ValidationErrors | null => {
+    const { minAmount, maxAmount, fromDate, toDate } = group.value;
+    const errors: ValidationErrors = {};
+
+    // Make sure the min amount does not exceed the max amount in the filter
+    if (minAmount && maxAmount && Number(minAmount) > Number(maxAmount)) {
+      errors['amountRange'] = true;
+    }
+
+    // Make sure the fromDate never chronologically exceeds the toDate
+    if (fromDate && toDate && fromDate > toDate) {
+      errors['dateRange'] = true;
+    }
+
+    return Object.keys(errors).length > 0 ? errors : null;
+  };
+
+  filterForm = this.fb.group(
+    {
+      minAmount: [''],
+      maxAmount: [''],
+      fromDate: [''],
+      toDate: [''],
+    },
+    { validators: [this.rangeValidator] },
+  );
+
   account() {
     return this.accountService.getAccountFromId(this.id());
+  }
+
+  hasActiveFilters() {
+    const { minAmount, maxAmount, fromDate, toDate } = this.appliedFilters();
+    return !!(minAmount || maxAmount || fromDate || toDate);
+  }
+
+  applyFilters() {
+    if (this.filterForm.invalid) return;
+    this.appliedFilters.set(this.filterForm.getRawValue());
+  }
+
+  clearFilters() {
+    this.filterForm.reset(NO_FILTERS);
+    this.appliedFilters.set(NO_FILTERS);
   }
 
   getTransactions() {
@@ -27,10 +92,24 @@ export class TransactionHistoryComponent {
     // history doesn't include the opening deposit, so back it out of the current balance
     let balance = account.balance - account.history.reduce((sum, t) => sum + effect(t), 0);
 
-    return account.history.map((transaction) => {
-      balance += effect(transaction);
-      return { transaction, balance };
-    });
+    return account.history
+      .map((transaction) => {
+        balance += effect(transaction);
+        return { transaction, balance };
+      })
+      .filter((entry) => this.matchesFilters(entry.transaction));
+  }
+
+  private matchesFilters(transaction: ITransaction) {
+    const { minAmount, maxAmount, fromDate, toDate } = this.appliedFilters();
+
+    const atLeastMin = !minAmount || transaction.amount >= Number(minAmount);
+    const atMostMax = !maxAmount || transaction.amount <= Number(maxAmount);
+
+    const onOrAfterFrom = !fromDate || transaction.date >= new Date(`${fromDate}T00:00:00`);
+    const onOrBeforeTo = !toDate || transaction.date <= new Date(`${toDate}T23:59:59.999`);
+
+    return atLeastMin && atMostMax && onOrAfterFrom && onOrBeforeTo;
   }
 
   getAccountFromId(id: string) {
