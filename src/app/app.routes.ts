6952a1d3698b1d085ad45942ea7a52e221b0c87c
Max Niu <maxniu444@gmail.com>
2026-09-07 16:29:01 -0400

    finishing touches

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { AccountCreationComponent } from './modules/features/accounts/account-cr
 import { AccountListComponent } from './modules/features/accounts/account-list/account-list.component';
 import { FundTransferComponent } from './modules/features/transactions/fund-transfer/fund-transfer.component';
 import { TransactionHistoryComponent } from './modules/features/transactions/transaction-history/transaction-history.component';
+import { NotFoundComponent } from './modules/shared/components/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'create-account', pathMatch: 'full' },
@@ -11,4 +12,7 @@ export const routes: Routes = [
   { path: 'accounts', component: AccountListComponent },
   { path: 'transfer', component: FundTransferComponent },
   { path: 'history/:id', component: TransactionHistoryComponent },
+
+  // Not found page
+  { path: '**', component: NotFoundComponent },
 ];
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -5,11 +5,24 @@ import { RouterModule } from '@angular/router';
 import { ButtonComponent } from './components/button/button.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { EmptyStateComponent } from './components/empty-state/empty-state.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 import { ValidCurrencyDirective } from './directives/valid-currency.directive';
 
 @NgModule({
-  declarations: [ButtonComponent, NavbarComponent, EmptyStateComponent, ValidCurrencyDirective],
+  declarations: [
+    ButtonComponent,
+    NavbarComponent,
+    EmptyStateComponent,
+    NotFoundComponent,
+    ValidCurrencyDirective,
+  ],
   imports: [CommonModule, RouterModule],
-  exports: [ButtonComponent, NavbarComponent, EmptyStateComponent, ValidCurrencyDirective],
+  exports: [
+    ButtonComponent,
+    NavbarComponent,
+    EmptyStateComponent,
+    NotFoundComponent,
+    ValidCurrencyDirective,
+  ],
 })
 export class SharedModule {}
